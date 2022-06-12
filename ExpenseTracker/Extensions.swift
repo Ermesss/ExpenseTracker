c56028ab5924b1345907df88eb3959c263b33ffa
Ermanno Fissore <ermannofissore@gmail.com>
2022-06-12 14:11:29 +0200

    Expense Tracker SwiftUI app

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Ermanno Fissore on 12/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color ("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
