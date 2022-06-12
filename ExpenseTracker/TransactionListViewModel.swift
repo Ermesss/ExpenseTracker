3c1afa21841acd811a1ed21e020c399d819c09a3
Ermanno Fissore <ermannofissore@gmail.com>
2022-06-12 17:36:32 +0200

    Fetch function

---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Ermanno Fissore on 12/06/2022.
+//
+
+import Foundation
+import Combine
+
+final class TransactionListViewModel: ObservableObject{
+    @Published var transaction: [Transaction] = []
+    
+    private var cancellable = Set<AnyCancellable>()
+    
+    func getTransactions(){
+        guard let url = URL(string: "https://designcode.io/data/transaction.json") else {
+            print("Invalid URL")
+            return
+        }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap {(data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    dump(response)
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error fetching transaction:", error.localizedDescription)
+                case .finished:
+                    print("Finished fetching transactions")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transaction = result
+                dump(self?.transaction)
+            }
+            .store(in: &cancellables)
+
+    }
+}
